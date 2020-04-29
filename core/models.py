74522fc4beb84d0658bec5f2f2d7182bd0e6c2df
Igor Sales <igorsales.fs@gmail.com>
2020-04-29 18:16:11 -0300

    adicionando interface home

--- core/models.py
+++ core/models.py
@@ -1,1 +1,14 @@
 from djongo import models
+
+
+class Veiculo(models.Model):
+
+   
+    placa = models.CharField(max_length=7, null=False)
+    proprietario = models.CharField(max_length=50, null=False)
+    matricula = models.BigIntegerField()
+    curso = models.CharField(max_length=50, null=False)
+    area_especial = models.BooleanField() 
+
+    def __str__(self):
+        return self.proprietario
--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from core.models import Veiculo
 
-# Create your views here.
+def home(request):
+    return render(request, 'index.html')
