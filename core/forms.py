ff56d7dd544d6bcfa801269be42a2beb6746fcbb
Rodrigo Andrade <rdghenrique94@gmail.com>
2020-06-02 13:51:09 -0300

    update
    update

--- core/forms.py
+++ core/forms.py
@@ -1,8 +1,14 @@
 from django.forms import ModelForm
 from core.models import Veiculo
+from core.models import Entrada
 
 
 class CadastroVeiculoForm(ModelForm):
     class Meta:
         model = Veiculo
         fields = ['placa', 'proprietario', 'matricula', 'curso', 'area_especial']
+
+class EntradaVeiculosForm(ModelForm):
+    class Meta:
+        model = Entrada
+        fields = ['setor_type','placa','data_joined']
--- core/models.py
+++ core/models.py
@@ -11,4 +11,15 @@ class Veiculo(models.Model):
 
     def __str__(self):
         return self.proprietario
+        
+class Entrada(models.Model):
+    SETOR_TYPE_CHOICES = (
+        (1, 'setor A Funcionarios'),
+        (2, 'setor B Geral'),
+        (3, 'setor C Geral'),
+        (4, 'setor D Geral'),
+    )
 
+    setor_type = models.PositiveSmallIntegerField(choices=SETOR_TYPE_CHOICES)
+    placa = models.CharField(max_length=7, null=False, unique=True)
+    date_joined = models.DateTimeField('Data de entrada', auto_now_add=True)
